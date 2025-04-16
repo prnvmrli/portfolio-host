e3bfdfa38aa46a01f986e4430b1b9aa20e8d92a4
Pranav Murali <pranavmurali@iouring.com>
2025-04-16 08:07:56 +0530

    home init

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1954936427"
+    serviceWorkerVersion: "1157209558"
   }
 });
